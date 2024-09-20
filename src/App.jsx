c8f888dad27ebe99ba9401cfbc13cf45025c4f7c
Migueel00 <rojassoriamiguelangel95@gmail.com>
2024-09-20 04:23:45 +0200

    [NEW]Index background, potions added, potions logid added, dificulty logic added

--- src/App.jsx
+++ src/App.jsx
@@ -3,112 +3,109 @@ import useSound from "use-sound";
 import simon from "./assets/sounds/sprite.mp3";
 import "./App.css";
 
-export default function App(){
-
-    // Referencias a los elementos de colores
-    const blueRef   = useRef(null);
-    const yellowRef = useRef(null);
-    const greenRef  = useRef(null);
-    const redRef    = useRef(null);
-
+export default function App() {
     // referencia al sprite de sonido
-    const [play ] = useSound(simon, {
+    const [play] = useSound(simon, {
         sprite: {
-            one:    [0, 500],
-            two:    [1000, 500],
-            three:  [2000, 500],
-            four:   [3000, 500],
-            erro:   [4000, 1000]
-        }
+            one: [0, 500],
+            two: [1000, 500],
+            three: [2000, 500],
+            four: [3000, 500],
+            erro: [4000, 1000],
+        },
     });
 
-    // Array de objetos para los colores, sus referencias y sonido
-    const colors = [
+    // Array de pociones
+    const potions = [
         {
-            color: '#FAF303',
-            ref:    yellowRef,
-            sound:  'one'
+            color: 'blue',
+            on: './public/blue-potion-on.png',
+            off: './public/blue-potion.png',
+            sound: 'one', // Si quieres asignar un sonido
         },
         {
-            color: '#030AFA',
-            ref:    blueRef,
-            sound: 'two'
+            color: 'green',
+            on: './public/green-potion-on.png',
+            off: './public/green-potion.png',
+            sound: 'two',
         },
         {
-            color: '#FA0E03',
-            ref:    redRef,
-            sound: 'three'
+            color: 'purple',
+            on: './public/purple-potion-on.png',
+            off: './public/purple-potion.png',
+            sound: 'three',
         },
         {
-            color: '#0AFA03',
-            ref:    greenRef,
-            sound: 'four'
-        }
+            color: 'red',
+            on: './public/red-potion-on.png',
+            off: './public/red-potion.png',
+            sound: 'four',
+        },
     ];
 
     // Constantes para los calculos del juego
     const minNumber = 0;
-    const maxNumber = 3;
+    const maxNumber = potions.length - 1; // Cambiado para que coincida con el número de pociones
     const speedGame = 400;
 
-
     // Hooks de estado
-    const [sequence, setSequence]               = useState([]);
-    const [currentGame, setCurrentGame]         = useState([]);
+    const [sequence, setSequence] = useState([]);
+    const [currentGame, setCurrentGame] = useState([]);
     const [isAllowedToPlay, setIsAllowedToPlay] = useState(false);
-    const [speed, setSpeed]                     = useState(speedGame);
-    const [turn, setTurn]                       = useState(0);
-    const [pulses, setPulses]                   = useState(0);
-    const [success, setSuccess]                 = useState(0);
-    const [isGameOn, setIsGameOn]               = useState(false);
+    const [speed, setSpeed] = useState(speedGame);
+    const [turn, setTurn] = useState(0);
+    const [pulses, setPulses] = useState(0);
+    const [success, setSuccess] = useState(0);
+    const [isGameOn, setIsGameOn] = useState(false);
+    const [activePotions, setActivePotions] = useState([]);
+    const [shuffledPotions, setShuffledPotions] = useState(potions);
 
     const initGame = () => {
         randomNumber();
         setIsGameOn(true);
-    }
+    };
 
     const randomNumber = () => {
         setIsAllowedToPlay(false);
-        const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-        setSequence([ ...sequence, randomNumber]);
+        const randomNum = Math.floor(Math.random() * (maxNumber + 1));
+        setSequence([...sequence, randomNum]);
         setTurn(turn + 1);
-    }
+    };
 
     const handleClick = (index) => {
-        if(isAllowedToPlay){
-            play({id: colors[index].sound})
-            colors[index].ref.current.style.opacity = (1);
-            colors[index].ref.current.style.scale   = (0.9);
-            setTimeout(() => {
-                colors[index].ref.current.style.opacity = (0.5);
-                colors[index].ref.current.style.scale   =(1);
-                setCurrentGame([ ...currentGame, index]);
-                setPulses(pulses + 1);
-
-            }, speed / 2)
+        if (isAllowedToPlay) {
+            play({ id: potions[index].sound });
+            setCurrentGame([...currentGame, index]);
+            setPulses(pulses + 1);
         }
+    };
+
+    const shufflePotions = (array) => {
+        let shuffledArray = [...array]
+
+        for(let i = shuffledArray.length - 1; i > 0; i--){
+
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]
+        }
+
+        return shuffledArray;
     }
 
     useEffect(() => {
-        if(pulses > 0){
-            if(Number(sequence[pulses -1]) === Number(currentGame[pulses - 1])){
+        if (pulses > 0) {
+            if (Number(sequence[pulses - 1]) === Number(currentGame[pulses - 1])) {
                 setSuccess(success + 1);
             } else {
-                const index = sequence[pulses - 1]
-                if(index) colors[index].ref.current.style.opacity = (1);
-                play({id: 'error'})
-                setTimeout(() => {
-                    if(index) colors[index].ref.current.style.opacity = (0.5);
-                    setIsGameOn(false);
-                }, speed * 2)
-            setIsAllowedToPlay(false);
+                const index = sequence[pulses - 1];
+                play({ id: 'erro' });
+                setIsGameOn(false);
             }
         }
-
     }, [pulses]);
 
     useEffect(() => {
-        if(!isGameOn){
+        if (!isGameOn) {
             setSequence([]);
             setCurrentGame([]);
             setIsAllowedToPlay(false);
@@ -116,68 +113,82 @@ export default function App(){
             setSuccess(0);
             setPulses(0);
             setTurn(0);
+            setActivePotions([]); 
         }
-    }, [isGameOn])
+    }, [isGameOn]);
 
     useEffect(() => {
-        if(success === sequence.length && success > 0){
+        if (success === sequence.length && success > 0) {
             setSpeed(speed - sequence.length * 2);
             setTimeout(() => {
                 setSuccess(0);
                 setPulses(0);
                 setCurrentGame([]);
                 randomNumber();
-            }, 500)
+            }, 500);
         }
     }, [success]);
 
     useEffect(() => {
-        if(!isAllowedToPlay){
-            sequence.map((item, index) => {
+        if (!isAllowedToPlay) {
+            sequence.forEach((item, index) => {
                 setTimeout(() => {
-                    play({id : colors[item].sound})
-                    colors[item].ref.current.style.opacity = (1);
+                    play({ id: potions[item].sound });
+                    setActivePotions(prev => [...prev, item]); 
                     setTimeout(() => {
-                        colors[item].ref.current.style.opacity = (0.5);
-                    }, speed / 2)
+                        setActivePotions(prev => prev.filter(i => i !== item)); 
+                    }, speed / 2);
                 }, speed * index);
             });
         }
         setIsAllowedToPlay(true);
-    }, [sequence])
+    }, [sequence]);
 
-    return(
+    useEffect(() => {
+        if (!isGameOn) {
+            document.body.style.backgroundImage = "url('./src/assets/images/background.jpg')";
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundPosition = 'center';
+        } else {
+            document.body.style.background = "url('./fondo-mesa.png')";
+            document.body.style.backgroundSize = 'cover';
+            document.body.style.backgroundPosition = 'center';
+        }
+    }, [isGameOn]);
+
+    useEffect(() => {
+        
+
+        if(turn > 0  && turn % 5 === 0){
+            const newShuffledPotions = shufflePotions(potions);
+            setShuffledPotions(newShuffledPotions)
+        }
+    }, [turn])
+
+    return (
         <>
-            {
-                isGameOn 
-                ?
+            {isGameOn ? (
                 <>
                     <div className="header">
                         <h1>Turn {turn}</h1>
                     </div>
                     <div className="container">
-                        {colors.map((item, index) => {
-                            return (
-                                <div    key = {index}
-                                        ref = {item.ref}
-                                        className = {`pad pad-${index}`} 
-                                        style = {{backgroundColor: `${item.color}`, opacity: 0.6}}
-                                        onClick = {() => handleClick(index)}
-                                >
-                                </div>
-                            );
-                        })}
+                        {shuffledPotions.map((item, index) => (
+                            <div key={index}  onClick={() => handleClick(index)}>
+                                <img
+                                    src={activePotions.includes(index) ? item.on : item.off}
+                                    alt={`${item.color} potion`}
+                                    className="potion-image"
+                                />
+                            </div>
+                        ))}
                     </div>
                 </>
-                :
-                <>
-                    <div className="header">
-                        <h1>SUPER SIMON</h1>
-                    </div>
-                    <button onClick={initGame}>START</button>
-                </>
-            }  
+            ) : (
+                <div className="header">
+                    <img src="./public/boton.png" alt="boton" onClick={initGame} className="btn-enter" />
+                </div>
+            )}
         </>
-    )
+    );
 }
-
